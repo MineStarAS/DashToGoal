3f1ef8a0e882ebcc327f0ca0edf8ddd278fefe4d
MineStar <band1019@naver.com>
2022-09-04 06:35:46 +0900

    # 기능 일부 추가

--- Assets/src/kr.kro.minestar/GameSystem.cs
+++ Assets/src/kr.kro.minestar/GameSystem.cs
@@ -1,10 +1,22 @@
 using System.Collections.Generic;
+using src.kr.kro.minestar.gameEvent;
 using src.kr.kro.minestar.player;
 
 namespace src.kr.kro.minestar
 {
-    public static class GameSystem
+    public class GameSystem
     {
-        public static HashSet<Player> Players = new ();
+        /// ##### Field #####
+        public GameEventOperator GameEventOperator;
+
+        public string PlayMap;
+        
+        public HashSet<Player> Players = new();
+
+        /// ##### Constructor #####
+        public GameSystem()
+        {
+            GameEventOperator = new GameEventOperator(this);
+        }
     }
 }
