e69ac2960513d673597056e7199c2e98700b500b
MineStar <band1019@naver.com>
2022-09-10 18:49:37 +0900

    # Player 수정

--- Assets/src/kr.kro.minestar/player/Player.cs
+++ Assets/src/kr.kro.minestar/player/Player.cs
@@ -12,23 +12,10 @@ namespace src.kr.kro.minestar.player
 {
     public class Player : MonoBehaviour
     {
-        /// ##### Constant Field #####
-        private const float MoveForce = 6F;
-        private const float JumpForce = 13F;
-        private const float GizmoSize = 5F;
-        private const byte DefaultAirJumpAmount = 2;
-
         /// ##### Field #####
         private readonly GameSystem _gameSystem;
 
         private readonly PlayerCharacter _playerCharacter;
-
-        private readonly Rigidbody2D _body; // 플레이어 물리
-        private SpriteRenderer _spriteRenderer; // 스프라이트 정보
-
-        private int _airJumpAmount; // 공중 점프 가능 횟수
-        private Transform _target; // 감지된 물체
-        private bool m_isLanding; // 땅에 닿았는가
 
         private readonly List<Effect> _effects = new ();
         [CanBeNull] private string _item;
@@ -40,37 +27,8 @@ namespace src.kr.kro.minestar.player
         {
             _gameSystem = gameSystem;
             _playerCharacter = PlayerCharacter.FromEnum(this, playerCharacterEnum);
-
-            m_isLanding = false;
-
-            _airJumpAmount = DefaultAirJumpAmount;
             // _effects = new List<Effect>();
-
-            // _body = GetComponent<Rigidbody2D>();
-            // _spriteRenderer = GetComponent<SpriteRenderer>();
-
-            _body = new Rigidbody2D();
-            _spriteRenderer = new SpriteRenderer();
         }
-
-        /// ##### Unity Functions #####
-        private void Update()
-        {
-            DoJump(); // 점프
-            DoMove(); // 좌우 이동
-            DoDetect(); // 범위내 오브젝트 감지
-        }
-
-        // private void FixedUpdate()
-        // {
-        //     // 플렛폼 타입 알아내기
-        //     Debug.DrawRay(_body.position, Vector3.down, new Color(0, 1, 0));
-        //     var rayHit = Physics2D.Raycast(_body.position, Vector3.down, 1, LayerMask.GetMask("Platform"));
-        //     var rayHitCollider = rayHit.collider;
-        //     
-        //     //if (rayHitCollider != null) Debug.Log(rayHitCollider.tag);
-        //     if (_body.velocity.y < -10.0f && !m_isLanding) _body.drag = 2.0f;
-        // }
 
         /// ##### Get Functions #####
         public PlayerCharacter GetPlayerCharacter() => _playerCharacter;
@@ -79,7 +37,7 @@ namespace src.kr.kro.minestar.player
 
         public float GetMoveForce()
         {
-            var value = MoveForce;
+            var value = 1f;
 
             if (_effects == null || _effects.Count == 0) return value;
 
@@ -128,7 +86,7 @@ namespace src.kr.kro.minestar.player
 
         public float GetJumpForce()
         {
-            var value = JumpForce;
+            var value = 1f;
 
             if (_effects == null || _effects.Count == 0) return value;
                 
@@ -193,59 +151,6 @@ namespace src.kr.kro.minestar.player
         }
 
         ///###### Do Functions #####
-        private void DoJump()
-        {
-            var value = GetJumpForce();
-
-            if (_airJumpAmount <= 0) return;
-            // 점프 횟수 추가
-            if (!(Input.GetKeyDown(KeyCode.Space) & _airJumpAmount != 0)) return;
-            _airJumpAmount--;
-
-            _body.velocity = Vector2.zero;
-            var jumpVelocity = new Vector2(0, value); // 점프 속력
-            _body.AddForce(jumpVelocity, ForceMode2D.Impulse);
-            m_isLanding = false;
-
-            var playerEvent = new PlayerJumpEvent(this, value);
-        }
-        
-        private void DoMove()
-        {
-            var value = GetMoveForce();
-            if (Input.GetKey(KeyCode.RightArrow))
-            {
-                _spriteRenderer.flipX = false;
-            }
-            else if (Input.GetKey(KeyCode.LeftArrow))
-            {
-                value *= -1;
-                _spriteRenderer.flipX = true;
-            }
-
-            _body.velocity = new Vector2(value, _body.velocity.y);
-        }
-        
-        private void DoDetect() // 플레이어 혹은 물체 감지 - 나중에 스킬로 옮기면됨
-        {
-            if (!Input.GetKeyDown(KeyCode.V)) return;
-            var cols = Physics2D.OverlapCircleAll(transform.position, GizmoSize);
-            if (cols.Length > 0)
-            {
-                foreach (var t in cols)
-                {
-                    if (!t.CompareTag("Enemy")) continue;
-                    Debug.Log("Physics Enemy : Target found");
-                    _target = t.gameObject.transform;
-                }
-            }
-            else
-            {
-                Debug.Log("Physics Enemy : Target lost");
-                _target = null;
-            }
-        }
-
 
         private void DoUseActiveSkill1()
         {
@@ -306,26 +211,8 @@ namespace src.kr.kro.minestar.player
                         continue;
                 }
             }
-
-            _airJumpAmount = value;
+            
             return value;
-        }
-
-        public void AddVector(Vector2 vector) => _body.AddForce(vector);
-
-        private void OnTriggerEnter2D(Collider2D other)
-        {
-            // 무한 점프 막기
-            if (other.gameObject.layer != 6 || !(_body.velocity.y <= 0)) return;
-            m_isLanding = true;
-            _airJumpAmount = DefaultAirJumpAmount;
-            _body.drag = 0;
-        }
-
-        private void OnDrawGizmos() // 감지 거리 그리기
-        {
-            Gizmos.color = Color.yellow;
-            Gizmos.DrawWireSphere(transform.position, GizmoSize);
         }
     }
 }
