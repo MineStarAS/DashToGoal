c77680dbec25a23597dd1500159ba7843131f4eb
MineStar <band1019@naver.com>
2022-09-21 03:25:57 +0900

    #

--- Assets/src/kr.kro.minestar/utility/ExpandFunction.cs
+++ Assets/src/kr.kro.minestar/utility/ExpandFunction.cs
@@ -1,15 +1,26 @@
 using System;
+using Microsoft.CSharp;
 
 namespace src.kr.kro.minestar.utility
 {
     public static class ExpandFunction
     {
-        public static void Debug(this object obj) => UnityEngine.Debug.Log(obj);
+        public static string Debug(this string str)
+        {
+            UnityEngine.Debug.Log(str);
+            return str;
+        }
         
-        public static void Debug(this object obj, string tag) => UnityEngine.Debug.Log(tag + obj);
-        
-        public static int ConvertToIntTime(this double doubleTime) => doubleTime <= 0 ? 0 : Convert.ToInt32(Math.Round(doubleTime, 2) * 100);
-        
-        public static double ConvertToDoubleTime(this int intTime) => Math.Round(intTime / 100.0, 1);
+        public static T Debug<T>(this object obj)
+        {
+            UnityEngine.Debug.Log(obj);
+            return obj is T obj1 ? obj1 : default;
+        }
+
+        public static T Debug<T>(this object obj, string tag)
+        {
+            UnityEngine.Debug.Log($"{tag}{obj}");
+            return obj is T obj1 ? obj1 : default;
+        }
     }
 }
